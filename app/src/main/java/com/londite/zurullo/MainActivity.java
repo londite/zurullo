71e53e5a400f255bb39d3e0c6c36ef59ba9025a6
Jose Garcia <j.ga.clavijo@gmail.com>
2015-04-03 23:48:40 +0100

    Submitting this before going to bed. Something fails (I added a couple of comments). Probably is something really stupid.

--- app/src/main/java/com/londite/zurullo/MainActivity.java
+++ app/src/main/java/com/londite/zurullo/MainActivity.java
@@ -1,39 +1,34 @@
 package com.londite.zurullo;
 
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //I don't know why is not grabbing the id of the button
+        Button buttonGo = (Button) findViewById(R.id.buttonGo);
+
+    }
+
+        //Is not grabbing the editText ID either. I probably forgot something
+    public void goButton (View v) {
+        EditText laURL=(EditText) findViewById(R.id.editTextURL);
+
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(laURL.getText().toString())));
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
